bb37a02f6bfffad3d2a55718d5f545ff88e50ed0
Evan Frabell <epfrabell@gmail.com>
2022-08-24 07:08:52 -0400

    s

--- src/components/Summary/index.vue
+++ src/components/Summary/index.vue
@@ -19,5 +19,5 @@
   <h4 class="tooltip">Numbers based on square mile radius</h4>
 </template>
 
-<script src="@/components/Summary/summary.js"></script>
+<script src="@/components/Summary/Summary.js"></script>
 <style src="./summary.scss" lang="scss"></style>
