dac0165784158d3e991237169a7dbc69dc5552bf
Evan Frabell <epfrabell@gmail.com>
2022-08-24 06:53:56 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -18,7 +18,7 @@ export default defineConfig(({ mode }) => {
     ],
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, 'resources/js'),
+        '@': path.resolve(__dirname, './src'),
       },
     },
     define: {
