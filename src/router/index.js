0989ee84f5080d24f93d2771f105a6f84588d77f
Jay Roseman <jaybit33@gmail.com>
2022-08-26 19:31:43 -0400

    auth

--- src/router/index.js
+++ src/router/index.js
@@ -63,35 +63,30 @@ const router = new createRouter({
   routes,
 });
 
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some((record) => record.meta.requiresAuth);
+  const isAuthenticated = getAuth().currentUser;
+  console.log('isauthenticated', isAuthenticated);
+  console.log('requiresAuth', requiresAuth);
+  if (requiresAuth && !isAuthenticated) {
+    next('/login');
+  } else {
+    next();
+  }
 
-
-
-// router.beforeEach((to, from, next) => {
-//   const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
-//   const isAuthenticated = getAuth().currentUser;
-//   console.log("isauthenticated", isAuthenticated);
-//   console.log('requiresAuth', requiresAuth);
-//   if (requiresAuth && !isAuthenticated) {
-//     next("/login");
-//   } else {
-//     next();
-//   }
-
-
-// const auth = getAuth();
-// onAuthStateChanged(auth, (user) => {
-//   if (user) {
-//     // User is signed in, see docs for a list of available properties
-//     // https://firebase.google.com/docs/reference/js/firebase.User
-//     const uid = user.uid;
-//     next("/login");
-//   } else {
-//     next("/login")
-//     // User is signed out
-//     // ...
-//   }
-// });
-
-// });
+  const auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/firebase.User
+      const uid = user.uid;
+      next('/login');
+    } else {
+      next('/login');
+      // User is signed out
+      // ...
+    }
+  });
+});
 
 export default router;
