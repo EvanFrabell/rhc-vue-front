555d43754e0465f30fa18584ae374a4e7e3d6c22
Jay Roseman <jaybit33@gmail.com>
2022-08-26 23:27:07 -0400

    new dist

---
+++ dist/assets/index.f232e71c.js
@@ -0,0 +1,1 @@
+import{_ as t,o,c as s,d as e}from"./index.dd2daae5.js";var n={name:"Home",components:{}};const a={class:"home"},i=e("div",{class:"home-title"},[e("h1",{id:"main-title"},"REAL HOME COMPARE"),e("h4",{id:"sub-title"},"Open Testing for Hamilton County")],-1),_=[i];function c(r,d,m,l,h,p){return o(),s("div",a,_)}var u=t(n,[["render",c]]);export{u as default};
